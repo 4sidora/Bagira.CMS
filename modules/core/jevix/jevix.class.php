36414b666b5a6e2dff6d4feaefed91a4253764e9
Alexander Gamaunov <unlike777@gmail.com>
2012-12-11 18:53:20 +0700

    Добавлено правило для правильной обработки ссылок mailto

--- modules/core/jevix/jevix.class.php
+++ modules/core/jevix/jevix.class.php
@@ -944,7 +944,7 @@ class Jevix{
 							continue(2);
 						}
 						// HTTP в начале если нет
-						if(!preg_match('/^(http|https|ftp):\/\//ui', $value) && !preg_match('/^(\/|\#)/ui', $value) ) $value = 'http://'.$value;
+						if(!preg_match('/^((http|https|ftp):\/\/)|(mailto:)/ui', $value) && !preg_match('/^(\/|\#)/ui', $value) ) $value = 'http://'.$value;
 						break;
 
 					case '#image':
